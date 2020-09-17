99cfdca86f20e3958de8d37c9e8a3cd5a114e1ff
Manoel Pereira <manoelg@ibm.com>
2020-09-17 11:20:07 -0300

    Created menu navbar

---
+++ client/src/components/NavBar/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="nav-bar">
+    <router-link class="nav-bar-item is-size-4" to="/">
+      <span class="nav-bar-item-text">Virtual Store</span>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+<style lang="scss">
+@import "~@shared/css/colors.scss";
+.nav-bar {
+  display: flex;
+  background-color: $soft;
+}
+.nav-bar-item {
+  text-decoration: none;
+}
+.nav-bar-item-text {
+  font-size: 2.5rem;
+  color: $white;
+  padding-left: 0.5rem;
+}
+</style>
