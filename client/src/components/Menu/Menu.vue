0d894760a64685e09b8cae674f279ca9cc5e5a37
Manoel Pereira <manoelg@ibm.com>
2020-09-28 15:37:47 -0300

    Created Menu component

---
+++ client/src/components/Menu/Menu.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="menu">
+    <router-link class="menu-item" :to="href">
+      <span class="menu-item-button-button">{{ nameButton }}</span>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "Menu",
+  props: {
+    href: { type: String, default: "#" },
+    nameButton: { type: String, required: true },
+  },
+};
+</script>
+<style lang="scss">
+@import "~@shared/css/colors.scss";
+.menu {
+  display: flex;
+  background-color: #ededed;
+}
+.menu-item {
+  text-decoration: none;
+  padding-right: 0.5rem;
+  align-self: center;
+  padding-left: 0.5rem;
+}
+.menu-item-button-button {
+  display: flex;
+  background-color: #0076ff;
+  font-size: 1.5rem;
+  border-radius: 0.3rem;
+  padding: 0.2rem;
+  color: $white;
+}
+.menu-item-button-button:hover {
+  border: solid 0.1rem #0076ff;
+  color: #0076ff;
+  background-color: white;
+}
+</style>
