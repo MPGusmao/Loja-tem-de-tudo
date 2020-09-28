0e538680670f27817ec16bfc076914b9c3d8e2e5
Manoel Pereira <manoelg@ibm.com>
2020-09-28 16:45:42 -0300

    Created HeaderTitle component

---
+++ client/src/components/SharedComponent/HeaderTitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header-title">
+    <span class="header-title-item">{{ title }}</span>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeaderTitle",
+  props: {
+    title: { type: String, default: null },
+  },
+};
+</script>
+<style lang="scss">
+@import "~@shared/css/colors.scss";
+.header-title {
+  display: flex;
+  background-color: #ededed;
+  justify-content: center;
+  border-top: solid 0.5rem white;
+  border-left: solid 1rem white;
+  border-right: solid 1rem white;
+}
+.header-title-item {
+  font-size: 2rem;
+  color: black;
+}
+</style>
