2d3c9970814b99d39a9f900ea96e6182321cc327
Manoel Pereira <manoelg@ibm.com>
2020-09-29 12:52:27 -0300

    Created produto and produto item component

---
+++ client/src/components/Produto/Produto.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="form-card">
+    <div class="form-card-content">
+      <div class="form-card-body">
+        <div v-for="(item, index) in produto" :key="index">
+          <ProdutoItem :item="item" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import ProdutoItem from "../Produto/ProdutoItem.vue";
+import InputField from "../SharedComponent/InputField.vue";
+export default {
+  nam: "Produto",
+  components: {
+    ProdutoItem,
+    InputField,
+  },
+  data() {
+    return {
+      produto: [
+        {
+          categoria: "Papelaria",
+          produto: "Lapis de cor azul",
+          descricao: "Lapis de cor azul caixa com 20 unidades",
+          valor: "R$ 20,00",
+        },
+        {
+          categoria: "Decoracao",
+          produto: "Abajur",
+          descricao: "Abajur com duas lampadas",
+          valor: "R$ 35,00",
+        },
+        {
+          categoria: "Papelaria",
+          produto: "Lapis de cor azul",
+          descricao: "Lapis de cor azul caixa com 20 unidades",
+          valor: "R$ 20,00",
+        },
+        {
+          categoria: "Papelaria",
+          produto: "Lapis de cor vermelho",
+          descricao: "Lapis de cor vermelho caixa com 20 unidades",
+          valor: "R$ 20,00",
+        },
+        {
+          categoria: "Papelaria",
+          produto: "Lapis de cor vermelho",
+          descricao: "Lapis de cor vermelho caixa com 20 unidades",
+          valor: "R$ 20,00",
+        },
+        {
+          categoria: "Papelaria",
+          produto: "Lapis de cor vermelho",
+          descricao: "Lapis de cor vermelho caixa com 20 unidades",
+          valor: "R$ 20,00",
+        },
+        {
+          categoria: "Papelaria",
+          produto: "Lapis de cor vermelho",
+          descricao: "Lapis de cor vermelho caixa com 20 unidades",
+          valor: "R$ 20,00",
+        },
+        {
+          categoria: "Papelaria",
+          produto: "Lapis de cor vermelho",
+          descricao: "Lapis de cor vermelho caixa com 20 unidades",
+          valor: "R$ 20,00",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss">
+.form-card {
+  display: flex;
+}
+.form-card-header {
+  display: flex;
+}
+.form-card-content {
+  display: flex;
+  background-color: #ededed;
+  margin: 0.5rem 1rem 0.5rem 1rem;
+  justify-content: space-between;
+  flex-direction: row;
+}
+.form-card-content-search {
+  display: flex;
+  align-items: flex-start;
+}
+.form-card-body {
+  display: flex;
+  padding: 2rem 2rem 2rem 2rem;
+  align-self: flex-start;
+  flex-wrap: wrap;
+  flex-grow: 1;
+}
+</style>
---
+++ client/src/components/Produto/ProdutoItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <router-link class="link" to="/produto">
+    <div class="produto-card">
+      <div class="produto-card-body">
+        <div class="produto-card-body-categoria">{{ item.categoria }}</div>
+        <div class="produto-card-body-item">{{ item.produto }}</div>
+        <div class="produto-card-body-item">{{ item.descricao }}</div>
+        <div class="produto-card-body-item">{{ item.valor }}</div>
+      </div>
+    </div>
+  </router-link>
+</template>
+<script>
+export default {
+  name: "ProdutoItem",
+  props: {
+    item: { type: Object },
+  },
+};
+</script>
+<style lang="scss">
+.link {
+  text-decoration: none;
+}
+.produto-card {
+  display: flex;
+  flex-direction: column;
+  width: 282px;
+  height: 160px;
+  background-color: #a3a1a1;
+  margin: 0.5rem;
+}
+.produto-card-body {
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  margin: 0.5rem;
+  overflow: hidden;
+  flex-wrap: wrap;
+}
+.produto-card-body-categoria {
+  display: flex;
+  flex-grow: 1;
+  font-size: 0.8rem;
+  font-weight: bold;
+  color: black;
+  align-self: center;
+}
+.produto-card-body-item {
+  display: flex;
+  flex-grow: 1;
+  font-size: 0.8rem;
+  font-weight: bold;
+  color: black;
+}
+</style>
