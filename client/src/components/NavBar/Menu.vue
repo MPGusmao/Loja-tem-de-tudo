ee37877413d78fb44c622dadffb87f8d82caf82d
Manoel Pereira <manoelg@ibm.com>
2020-10-16 22:07:26 -0300

    created menu label component

---
+++ client/src/components/NavBar/Menu.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="menu">
+    <router-link class="menu-item" :to="href">
+      <span class="menu-item-label">{{ label }}</span>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "Menu",
+  props: {
+    href: { type: String, default: "#" },
+    label: { type: String, required: true },
+  },
+};
+</script>
+<style lang="scss">
+@import "~@shared/css/colors.scss";
+.menu {
+  display: flex;
+}
+.menu-item {
+  text-decoration: none;
+  padding-right: 0.5rem;
+  align-self: center;
+  padding-left: 0.5rem;
+}
+.menu-item-label {
+  display: flex;
+  font-size: 1.2rem;
+  padding: 0.2rem;
+  color: #ededed;
+}
+.menu-item-label:hover {
+  color: black;
+}
+</style>
