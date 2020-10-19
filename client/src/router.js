c30073e90f0397eab0347e68f52ad9df4418be03
Manoel Pereira <manoelg@ibm.com>
2020-10-18 22:21:22 -0300

    Created produtoform router

--- client/src/router.js
+++ client/src/router.js
@@ -1,13 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import ProdutoForm from './Components/Produto/ProdutoForm.vue'
 import Produto from './components/Produto/Produto.vue'
-import VendedorForm from './components/Vendedor/VendedorForm.vue'
-import Venda from './components/Venda/Venda.vue'
-import RealizarVenda from './components/Venda/RealizarVenda.vue'
-import RelatorioVenda from './components/Venda/RelatorioVenda.vue'
-import ClienteForm from './components/Cliente/ClienteForm.vue'
-
+import ProdutoForm from './components/Produto/ProdutoForm.vue'
 
 Vue.use(Router);
 
@@ -15,40 +9,16 @@ const router = new Router({
     linkActiveClass: 'is-active',
     routes: [
         {
-            path: '/produto',
-            name: 'ProdutoForm',
-            component: ProdutoForm
-        },
-        {
             path: '/',
             name: 'Produto',
             component: Produto
         },
         {
-            path: '/vendedorform',
-            name: 'VendedorForm',
-            component: VendedorForm
-        },
-        {
-            path: '/venda',
-            name: 'Venda',
-            component: Venda
-        },
-        {
-            path: '/realizarvenda',
-            name: 'RealizarVenda',
-            component: RealizarVenda
-        },
-        {
-            path: '/relatoriovenda',
-            name: 'RelatorioVenda',
-            component: RelatorioVenda
-        },
-        {
-            path: '/clienteform',
-            name: 'ClienteForm',
-            component: ClienteForm
+            path: '/produtoform',
+            name: 'ProdutoForm',
+            component: ProdutoForm
         }
+
     ]
 });
 
