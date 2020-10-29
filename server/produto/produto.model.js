559e286989f34cd586ed6f49692a2d1814589a3a
Manoel Pereira <manoelg@ibm.com>
2020-10-28 23:48:49 -0300

    Added two news procedure to create new product and display all products in the front end

---
+++ server/produto/produto.model.js
@@ -0,0 +1,64 @@
+const responses = require('./produto.response.json')
+
+const createProduct = async (body, connection) => {
+    try {
+        if (!body) {
+            throw new Error(`Body is not defined: ${body}`)
+        }
+        const query = `
+            INSERT INTO 
+                XCV90760.PRODUTO
+                (
+                    NOME_PRODUTO,
+                    DESCRICAO,
+                    MARCA,
+                    FORNECEDOR,
+                    CLASSIFICACAO,
+                    PRECO_CUSTO,
+                    PRECO_VENDA,
+                    QTDE_LOJA,
+                    QTDE_ESTOQUE
+                )
+                values
+                (
+                    '${body.NOME_PRODUTO}',
+                    '${body.DESCRICAO}',
+                    '${body.MARCA}',
+                    '${body.FORNECEDOR}',
+                    '${body.CLASSIFICACAO}',
+                    '${body.PRECO_CUSTO}',
+                    '${body.PRECO_VENDA}',
+                    '${body.QTDE_LOJA}',
+                    '${body.QTDE_ESTOQUE}'
+                );`;
+        const result = await connection.query(query)
+        return { data: result, ...responses.CREATE_PRODUCT.success };
+    } catch (error) {
+        return { error, ...responses.CREATE_PRODUCT.error };
+    }
+};
+
+const getAllProduct = async (connection) => {
+    try {
+        const query = `
+            SELECT
+                NOME_PRODUTO,
+                DESCRICAO,
+                MARCA,
+                FORNECEDOR,
+                CLASSIFICACAO,
+                PRECO_CUSTO,
+                PRECO_VENDA,
+                QTDE_LOJA,
+                QTDE_ESTOQUE
+            FROM XCV90760.PRODUTO;`;
+        const result = await connection.query(query)
+        return { data: result, ...responses.GET_ALL_PRODUCT.success };
+    } catch (error) {
+        return { error, ...responses.GET_ALL_PRODUCT.error };
+    }
+};
+module.exports = {
+    createProduct,
+    getAllProduct
+}
