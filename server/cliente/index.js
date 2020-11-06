27389eb5d6005a0c198a4f53ab6a21d194597e03
Manoel Pereira <manoelg@ibm.com>
2020-11-05 22:21:35 -0300

    Created new Api

---
+++ server/cliente/index.js
@@ -0,0 +1,9 @@
+const controller = require('./cliente.controller.js');
+
+module.exports = (Router) => {
+    const router = Router();
+
+    router.post('/create', controller.createClient);
+
+    return router;
+};
