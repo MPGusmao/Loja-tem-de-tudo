c2cf969023f4c8e413e5afa0f8ef4a6cd2009e1e
Guilherme Oka <goka@Guilhermes-MacBook-Pro.local>
2020-12-16 22:50:04 -0300

    feat: updated auth module using passport-local

--- server/auth/auth.setup.js
+++ server/auth/auth.setup.js
@@ -1,29 +1,55 @@
-const passportLocal = require('passport-local').Strategy;
+const { Strategy } = require("passport-local");
+const db2 = require("../database/db2");
 const serializationResolver = (obj, done) => done(null, obj);
 
-const getStrategy = (config) => {
-    function strategy() {
-        return new Strategy(
-            function (username, password, cb) {
-                db.users.findByUsername(username, function (err, user) {
-                    if (err) { return cb(err); }
-                    if (!user) { return cb(null, false); }
-                    if (user.password != password) { return cb(null, false); }
-                    return cb(null, user);
-                });
-            })
-    }
+const getDB = config => {
+    const generator = db2(config);
+    return {
+        getConnection: generator.getConnection
+    };
+};
 
-    return strategy;
+const getUser = async (connection, email, password, callback) => {
+    try {
+        const query = `
+            SELECT
+                EMAIL,
+                PASSWORD
+            FROM XCV90760.LOGIN
+            WHERE 
+                EMAIL = ${email};`;
+        const result = await connection.query(query);
+        const user = result[0] || false;
+        callback(null, user);
+    } catch (error) {
+        callback(error, null);
+    }
+};
+
+const getStrategy = config => {
+    const db = getDB(config);
+    return new Strategy(function(username, password, cb) {
+        getUser(db.getConnection(), username, password, function(err, user) {
+            if (err) {
+                return cb(err);
+            }
+            if (!user) {
+                return cb(null, false);
+            }
+            if (user.password != password) {
+                return cb(null, false);
+            }
+            return cb(null, user);
+        });
+    });
 };
 
 module.exports = (app, passport, config) => {
-    const strategy = getStrategy(config);
     app.use(passport.initialize());
     app.use(passport.session());
 
     passport.serializeUser(serializationResolver);
     passport.deserializeUser(serializationResolver);
 
-    passport.use(strategy());
+    passport.use(getStrategy(config));
 };
